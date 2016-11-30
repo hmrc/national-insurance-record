cb70d5d3daaa455731484341416c241971f9673f
Prashant Choudhari <prashant.choudhari@digital.hmrc.gov.uk>
2016-11-30 14:08:34 +0000

    NISP-2145: Fixed review comments added 2 more exclusioon

--- app/uk/gov/hmrc/nationalinsurancerecord/controllers/ErrorResponses.scala
+++ app/uk/gov/hmrc/nationalinsurancerecord/controllers/ErrorResponses.scala
@@ -22,8 +22,12 @@ object ErrorResponses {
   val CODE_INVALID_NINO = "ERROR_NINO_INVALID"
   val CODE_MANUAL_CORRESPONDENCE = "EXCLUSION_MANUAL_CORRESPONDENCE"
   val CODE_DEAD = "EXCLUSION_DEAD"
+  val CODE_ISLE_OF_MAN = "ISLE_OF_MAN"
+  val CODE_MARRIED_WOMENS_REDUCED_RATE = "MARRIED_WOMENS_REDUCED_RATE"
 
   object ErrorNinoInvalid extends ErrorResponse(400, CODE_INVALID_NINO, "The provided NINO is not valid")
   object ExclusionManualCorrespondence extends ErrorResponse(403, CODE_MANUAL_CORRESPONDENCE, "The customer cannot access the service, they should contact HMRC")
-  //object ExclusionDead extends ErrorResponse(403, CODE_DEAD, "The customer needs to contact the National Insurance helpline")
+  object ExclusionDead extends ErrorResponse(403, CODE_DEAD, "The customer needs to contact the National Insurance helpline")
+  object ExclusionIsleOfMan extends ErrorResponse(403, CODE_ISLE_OF_MAN, "The customer needs to contact the National Insurance helpline")
+  object ExclusionMarriedWomenReducedRate extends ErrorResponse(403, CODE_MARRIED_WOMENS_REDUCED_RATE, "The customer needs to contact the National Insurance helpline")
 }
