7780e2e9a539db68e2d628eee512c1a5a306ce8b
Callum Turnbull <callum.turnbull@digital.hmrc.gov.uk>
2017-01-06 11:30:28 +0000

    Update Licence Headers

--- app/uk/gov/hmrc/nationalinsurancerecord/events/NationalInsuranceRecord.scala
+++ app/uk/gov/hmrc/nationalinsurancerecord/events/NationalInsuranceRecord.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 HM Revenue & Customs
+ * Copyright 2017 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -47,4 +47,3 @@ class NationalInsuranceRecord(nino: Nino, qualifyingYears: Int, qualifyingYearsP
     )
 
   )
-
