a864c7effff5edd0b25de1d6c533e10eb4a872dd
Vincent Vinnicombe <5462800+vinnicombe@users.noreply.github.com>
2022-07-04 08:27:15 +0100

    mongo-date-parse-fix: Drop DesLiabilitiesCache on start up

--- app/uk/gov/hmrc/nationalinsurancerecord/cache/DesLiabilitiesCache.scala
+++ app/uk/gov/hmrc/nationalinsurancerecord/cache/DesLiabilitiesCache.scala
@@ -17,34 +17,50 @@
 package uk.gov.hmrc.nationalinsurancerecord.cache
 
 import com.google.inject.{Inject, Singleton}
+import org.bson.types.ObjectId
 import org.joda.time.DateTime
-import play.api.libs.json.Json
+import play.api.libs.json.{Format, Json, OFormat}
 import uk.gov.hmrc.mongo.MongoComponent
 import uk.gov.hmrc.mongo.play.json.formats.{MongoFormats, MongoJodaFormats}
 import uk.gov.hmrc.nationalinsurancerecord.config.ApplicationConfig
 import uk.gov.hmrc.nationalinsurancerecord.domain.APITypes
 import uk.gov.hmrc.nationalinsurancerecord.domain.des.DesLiabilities
 import uk.gov.hmrc.nationalinsurancerecord.services.{CachingModel, CachingMongoService, MetricsService}
+
 import scala.concurrent.ExecutionContext.Implicits.global
 
-case class DesLiabilitiesCache(key: String, response: DesLiabilities, expiresAt: DateTime)
-  extends CachingModel[DesLiabilitiesCache, DesLiabilities]
+case class DesLiabilitiesCache(
+  key: String,
+  response: DesLiabilities,
+  expiresAt: DateTime
+) extends CachingModel[DesLiabilitiesCache, DesLiabilities]
 
 object DesLiabilitiesCache {
-  implicit val dateFormat = MongoJodaFormats.dateTimeFormat
-  implicit val idFormat = MongoFormats.objectIdFormat
-  implicit def formats = Json.format[DesLiabilitiesCache]
+  implicit val dateFormat: Format[DateTime] = MongoJodaFormats.dateTimeFormat
+  implicit val idFormat: Format[ObjectId] = MongoFormats.objectIdFormat
+
+  implicit def formats: OFormat[DesLiabilitiesCache] = Json.format[DesLiabilitiesCache]
 }
 
 //TODO: look to extend CachingMongoService?
 @Singleton
-class DesLiabilitiesRepository @Inject()( mongoComponent: MongoComponent,
-                                          metricsService: MetricsService,
-                                         applicationConfig: ApplicationConfig) {
+class DesLiabilitiesRepository @Inject()(
+  mongoComponent: MongoComponent,
+  metricsService: MetricsService,
+  applicationConfig: ApplicationConfig
+) {
 
-  private lazy val cacheService = new CachingMongoService[DesLiabilitiesCache, DesLiabilities](
-    mongoComponent, DesLiabilitiesCache.formats, DesLiabilitiesCache.apply, APITypes.Liabilities, applicationConfig, metricsService
+  private val cacheService = new CachingMongoService[DesLiabilitiesCache, DesLiabilities](
+    mongo = mongoComponent,
+    formats = DesLiabilitiesCache.formats,
+    apply = DesLiabilitiesCache.apply,
+    apiType = APITypes.Liabilities,
+    appConfig = applicationConfig,
+    metricsX = metricsService
   )
+
+  // TODO remove once https://jira.tools.tax.service.gov.uk/browse/DDCNL-6141 is marked as DONE
+  cacheService.collection.drop()
 
   def apply(): CachingMongoService[DesLiabilitiesCache, DesLiabilities] = cacheService
 }
