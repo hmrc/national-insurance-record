5e64ce77eca081a27bd02d4498edd2963ece51a9
AndrewBrown22 <45789050+andrewbrown22@users.noreply.github.com>
2025-08-12 14:44:14 +0100

    DDCNL-10882 fix errors i made

--- it/test/uk/gov/hmrc/nationalinsurancerecord/controllers/nationalInsurance/NationalInsuranceRecordControllerISpec.scala
+++ it/test/uk/gov/hmrc/nationalinsurancerecord/controllers/nationalInsurance/NationalInsuranceRecordControllerISpec.scala
@@ -48,16 +48,10 @@ trait NationalInsuranceRecordControllerISpec extends IntegrationBaseSpec with Re
   override def beforeEach(): Unit = {
     super.beforeEach()
 
-    def generateAuthHeaderResponse: String =
+    val authResponse =
       s"""
          |{
          |  "nino": "$nino",
-         |  "trustedHelper": {
-         |    "principalName": "Mr Test",
-         |    "attorneyName": "Mr Test",
-         |    "returnLinkUrl": "http://test.com/",
-         |    "principalNino": "$nino"
-         |  },
          |  "authProviderId": {
          |    "ggCredId": "xyz"
          |  },
@@ -73,17 +67,17 @@ trait NationalInsuranceRecordControllerISpec extends IntegrationBaseSpec with Re
          |}
          |""".stripMargin
 
-    stubPostServer(ok(generateAuthHeaderResponse), "/auth/authorise")
+    stubPostServer(ok(authResponse), "/auth/authorise")
     stubPostServer(ok(pertaxAuthResponse), "/pertax/authorise")
     stubGetServer(ok(""), s"/citizen-details/${nino.nino}/designatory-details/")
   }
 
   private val requests = List(
-    badRequest() -> "400" -> BAD_REQUEST,
-    unauthorized() -> "401" -> BAD_GATEWAY,
-    notFound() -> "404" -> NOT_FOUND,
-    serverError() -> "500" -> BAD_GATEWAY,
-    badGateway() -> "502" -> BAD_GATEWAY,
+    badRequest()     -> "400" -> BAD_REQUEST,
+    unauthorized()   -> "401" -> BAD_GATEWAY,
+    notFound()       -> "404" -> NOT_FOUND,
+    serverError()    -> "500" -> BAD_GATEWAY,
+    badGateway()     -> "502" -> BAD_GATEWAY,
     gatewayTimeout() -> "504" -> GATEWAY_TIMEOUT
   )
 
