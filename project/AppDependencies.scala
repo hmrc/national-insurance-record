3c198e3236e3ae267a6d7fc5207fb835525860ba
Vincent Vinnicombe <5462800+vinnicombe@users.noreply.github.com>
2026-05-12 08:49:25 +0100

    DDCNL-11945: PR bot comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -18,7 +18,7 @@ import play.sbt.PlayImport.*
 import sbt.*
 
 object AppDependencies {
-  private val bootstrapVersion = "10.5.0"
+  private val bootstrapVersion = "10.7.0"
   private val playVersion = "play-30"
   private val hmrcMongoVersion = "2.12.0"
   private val domainVersion = "13.0.0"
